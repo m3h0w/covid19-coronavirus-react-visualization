cb461a7f82b67fadc329e2623e95101cf253f69a
m3h0w <michalgacka@gmail.com>
2020-03-26 18:16:36 +0100

    Fix Taiwan flag not displaying

--- src/utils/countryToCode.ts
+++ src/utils/countryToCode.ts
@@ -1,7 +1,7 @@
 import countryCodes from '../data/countryCodes.json';
 
 const countryToCode = (country: string) => {
-  return countryCodes.find((v) => v.country_name === country)?.country_code;
+  return countryCodes.find((v) => v.country_name === country.replace('*', ''))?.country_code;
 };
 
 export default countryToCode;
